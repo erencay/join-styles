ad4eaf36f4959d809516a8bafb02ca61ce4640d2
PsiCat <ogealter@gmail.com>
2017-05-13 13:24:55 +0300

    Support overrides

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 function joinStyles() {
   var args = Array.prototype.slice.call(arguments);
-  var styles = args.shift();
-  var classNames = '';
+  var styles = { };
 
+  if( (typeof args[0] === "object") && (args[0] !== null) ) {
+    styles = args.shift();
+  }
+
+  var classNames = [];
 
   for (var i = 0; i < args.length; i++) {
     var className = styles[args[i]] || args[i];
 
-    classNames += className + ' ';
+    if (!className) {
+      continue;
+    }
+
+    if (classNames.includes(className)) {
+      className = args[i];
+    }
+
+    classNames.push(className);
   }
 
-  return classNames.replace(/\s+$/g,'');
+  return classNames.join(' ');
 }
 
 module.exports = joinStyles;
--- test.js
+++ test.js
@@ -1,12 +1,21 @@
 var assert = require('assert');
 var joinStyles = require('./index.js');
 
-var styles = {
-  class1: '.abc',
-  class2: '.def',
-};
+(function () {
+  var styles = {
+    class1: 'abc',
+    class2: 'def',
+  };
 
-var actual = joinStyles(styles, 'class1', 'class2', '.global');
-var expected = '.abc .def .global';
+  var actual = joinStyles(styles, 'class1', 'class1', 'class2', 'global', null, undefined);
+  var expected = 'abc class1 def global';
 
-assert.equal(actual, expected);
+  assert.equal(actual, expected);
+})();
+
+(function () {
+  var actual = joinStyles('.global1', '.global2');
+  var expected = '.global1 .global2';
+
+  assert.equal(actual, expected);
+})();
